1dbc97c1dfd57ade65571084d228cf000a0e851f
MarikOfAces <oliwalters@hotmail.co.uk>
2016-05-01 23:23:33 +0100

    Map update, Hay bales, Health, Stamina, falling damage, climbing areas

--- 3d Stealth game - AI.1/Assets/Scripts/Player/PlayerMovement.cs
+++ 3d Stealth game - AI.1/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
-
+using System.Timers;
+using UnityEngine;
+using System.Collections;
+using System;
 public class PlayerMovement : MonoBehaviour {
-
-    public Rigidbody rb;
-    public float speed;
-    public Vector3 velocity;
-    public float Noise;
-
+	public Rigidbody rb;
+	public float speed;
+	public Vector3 velocity;
+	public float Noise;
 	public bool canClimb;
 	public bool climbMode;
 	public bool isClimbing = false;
 	float mouseSensitivity = 5.0f;
-
 	RaycastHit hasHit;
 	Vector3 climbVector;
 
+	float Health;
+	float Stamina;
+	float fallTimer;
+	bool falling;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+		rb = GetComponent<Rigidbody>();
 		speed = 0.1f;
 	}
 	
@@ -33,28 +37,26 @@ public class PlayerMovement : MonoBehaviour {
 		//Debug.DrawRay(transform.position, transform.right * 1.0f, Color.black);
 		//Debug.DrawRay(transform.position, -transform.right * 1.0f, Color.black);
 	}
-
-    void FixedUpdate()
-    { 
+	void FixedUpdate()
+	{
 		Vector3 rayPos = transform.position - new Vector3 (0, 0.5f, 0);
 		if ((Physics.Raycast (rayPos, transform.forward, out hasHit, 1.0f)) && (hasHit.collider.tag == "Climb")) {
 			canClimb = true;		
 			print ("canClimb = true");
 		}else {
-			canClimb = false;	
+			canClimb = false;
+			
 		}
 		if (canClimb == false) {
 			//climbMode = false;
 		}
-
-
 		if (canClimb && playerMovementController.useAction) {
 			climbMode = true;
 			print ("ClimbMode = true");
 		}
 		//if (playerMovementController.useAction && isClimbing) {
-			//print ("climb OFF");
-			//climbMode = false;
+		//print ("climb OFF");
+		//climbMode = false;
 		//}
 		if (!canClimb){
 			print("Disconected from wall");
@@ -71,12 +73,11 @@ public class PlayerMovement : MonoBehaviour {
 			transform.Translate(new Vector3(moveHorizontal,moveVertical,0));
 			print ("Climbmode");
 			gameObject.GetComponent<Rigidbody>().useGravity = false;
-			if (playerMovementController.useAction){
-				print ("leave wall");
-				climbMode = false;
-				canClimb = false;
-			}
-
+			//if (playerMovementController.useAction){
+			//	print ("leave wall");
+			//	climbMode = false;
+			//	canClo
+			//}
 		} else {
 			gameObject.GetComponent<Rigidbody>().useGravity = true;
 			float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -92,12 +93,11 @@ public class PlayerMovement : MonoBehaviour {
 			float rotY = Input.GetAxis ("Mouse X") * mouseSensitivity;
 			transform.Rotate (0, rotY, 0);
 		}
-    }
-
-    internal void updatePlayerHp(int guardDmg)
-    {
-        throw new NotImplementedException();
-    }
+	}
+	internal void updatePlayerHp(int guardDmg)
+	{
+		throw new NotImplementedException();
+	}
 	void ControllPlayer()
 	{
 		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
@@ -112,6 +112,44 @@ public class PlayerMovement : MonoBehaviour {
 		
 		
 		//transform.Translate (movement * speed * Time.deltaTime, Space.World);
+	}
 
+	void fallDamage()
+	{
+	if (rb.useGravity) {
+			Debug.DrawRay (gameObject.transform.position, -Vector3.up * 1.0f, Color.black);
+			if (!(Physics.Raycast (gameObject.transform.position, -transform.up, out hasHit, 0.55f))) {
+				falling = true;
+			} else {
+				falling = false;
+			}
+			if (falling) {
+				fallTimer += 0.1f;
+			} else if (!falling) {
+				Physics.Raycast (gameObject.transform.position, -transform.up, out hasHit, 0.6f);
+				if (!(hasHit.collider.tag == "Soft Land")) {
+					if (fallTimer > 0.99f) {
+						Health -= fallTimer;
+				
+					}
+					fallTimer = 0;
+				} else {
+					fallTimer = 0;
+				}
+			}
+
+		} else {
+			fallTimer = 0; 
+		}
+	}
+
+
+
+
+
+	void playerDeath (){
+	if (Health <= 0) {
+			Destroy (gameObject);
+		}
 	}
 }
